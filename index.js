4a926685c33416e3b00c1778b8aa5d2f0007b355
elmar1huan8 <elmar1huan8@icloud.com>
2023-08-13 07:46:31 -0700

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-// Import stylesheets
-import './style.css';
+const http = require('http');
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
